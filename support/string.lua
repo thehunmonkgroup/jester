aa34e61ad9902b50a18af50ae1ddc649e0631487
Chad Phillips <chad@apartmentlines.com>
2010-12-16 23:51:04 -0700

    adding support for newlines to wrap function.

--- support/string.lua
+++ support/string.lua
@@ -19,39 +19,43 @@ function string:wrap(boundary, indent)
   local lines = self:split("\n") 
   
   for _, line in pairs(lines) do
-    words = line:split(" ")
-    leading_space = line:match("^(%s+)%S")
-    if (#words > 0) then
-      if leading_space then
-        full_indent = leading_space .. indent
-      else
-        full_indent = indent
-      end
-      index = 1
-      while words[index] do
-        local word = " " .. words[index]
-        if (buffer:len() >= boundary) then
-          table.insert(output, buffer:sub(1, boundary))
-          buffer = full_indent .. buffer:sub(boundary + 1)
+    if line:match("^%s*$") then
+      table.insert(output, "")
+    else
+      words = line:split(" ")
+      leading_space = line:match("^(%s+)%S")
+      if (#words > 0) then
+        if leading_space then
+          full_indent = leading_space .. indent
         else
-          if (word:len() > boundary) then
-            buffer = buffer .. word
-          elseif (buffer:len() + word:len() >= boundary) then
-            table.insert(output, buffer)
-            buffer = full_indent
+          full_indent = indent
+        end
+        index = 1
+        while words[index] do
+          local word = " " .. words[index]
+          if (buffer:len() >= boundary) then
+            table.insert(output, buffer:sub(1, boundary))
+            buffer = full_indent .. buffer:sub(boundary + 1)
           else
-            if (buffer == full_indent) then
-              buffer = full_indent .. word:sub(2)
-            else
+            if (word:len() > boundary) then
               buffer = buffer .. word
+            elseif (buffer:len() + word:len() >= boundary) then
+              table.insert(output, buffer)
+              buffer = full_indent
+            else
+              if (buffer == full_indent) then
+                buffer = full_indent .. word:sub(2)
+              else
+                buffer = buffer .. word
+              end
+              index = index + 1
             end
-            index = index + 1
           end
         end
-      end
-      if (buffer:match("%S")) then
-          table.insert(output, buffer)
-          buffer = indent
+        if (buffer:match("%S")) then
+            table.insert(output, buffer)
+            buffer = indent
+        end
       end
     end
   end
