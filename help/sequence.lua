9c65a374e943220fd53d25e4e50d5d2d7be466ff
Chad Phillips <chad@apartmentlines.com>
2010-12-18 01:58:59 -0700

    deleting dead help files.

--- help/sequence.lua
+++
@@ -1,9 +0,0 @@
-return [[
-A sequence is a series of actions that are executed in a sequential order.
-
-Sequences are the primary control mechanism for building workflows in Jester.
-
-SUBTOPICS:
-variables:
-  How to deal handle variable data.
-]]
--- help/sequence_variables.lua
+++
@@ -1,9 +0,0 @@
-return [[
-Sequences can use variables from four places:
-
- 1. Global configuration
- 2. Profile configuration
- 3. Channel variables
- 4. Jester's internal storage system
-]]
-
