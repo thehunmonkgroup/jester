b0a654a042058e95119929b9f42493021bd5c025
Chad Phillips <chad@apartmentlines.com>
2010-12-18 12:53:36 -0700

    switch jsequence to use a space separated list instead of comma separated, for consistency with how it's used in argument form.  add a special case to pass a 'keys' argument to get just a keys parameter template.

--- help/scripts.lua
+++ help/scripts.lua
@@ -25,7 +25,7 @@ jester.help_map.scripts.jsequence = {}
 jester.help_map.scripts.jsequence.description_short = [[Easily generate properly formatted sequence templates or action fragments with one command.]]
 jester.help_map.scripts.jsequence.description_long = [[jsequence removes the major headache of getting initial sequence templates laid out.  Probably the most frustrating part of building sequences is dealing with syntax errors due to improper formatting, and this script eliminates many of those issues.
 
-When called with no arguments, it brings up a dialog asking a few simple questions which it uses to generate the template.  Alternatively, it can be called with a variable number of arguments, each the name of an action, and it will output just the structure for the passed actions, which can then be copied or piped into an existing sequence.
+When called with no arguments, it brings up a dialog asking a few simple questions which it uses to generate the template.  Alternatively, it can be called with a variable number of arguments, each the name of an action, and it will output just the structure for the passed actions, which can then be copied or piped into an existing sequence.  Or, when called with just the argument 'keys', it will output the template for the rather complex 'keys' parameter.
   
 To install:
   1. Follow the instructions in the main Jester INSTALL.txt for properly setting your LUA_PATH environment variable.
