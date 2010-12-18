872183513b53d0cd14dbd9424f4d8d7cbbc01388
Chad Phillips <chad@apartmentlines.com>
2010-12-18 01:58:22 -0700

    revamping actions and modules help in general help system to be placeholders for the main topic page.

--- help/actions.lua
+++ help/actions.lua
@@ -1,5 +1,9 @@
-jester.help.action_summary = {}
-jester.help.action_summary.description_long = [[Actions are the building blocks of Jester that are provided by the various modules that are installed.  Generally speaking, an action's job is perform one discrete task very well.  Most actions accept parameters so that they can be used more flexibly.  From a programmer's perspective, actions are functions that accept arguments.]]
+--[[
+  Action help is stored with modules, and available at 'help actions'.  This
+  is merely a placeholder for the table of contents and help page summary.
+]]
+jester.help_map.actions = {}
+jester.help_map.actions.description_short = [[View the list of available actions.  Run 'help action [name]' to get more help on a specific action.]]
+jester.help_map.actions["See 'help actions'"] = {}
+jester.help_map.actions["See 'help actions'"].description_short = [[]]
 
-jester.help.action_summary.subtopic1 = {}
-jester.help.action_summary.subtopic1.description_short = [[here is a short desc.]]
--- help/modules.lua
+++ help/modules.lua
@@ -1,16 +1,9 @@
-return [[
-Modules are written by Jester developers.  They are what makes actions
-available to the sequences you write.  Most modules have a small collection
-of related actions (for example, the 'play' module provides actions which play
-sounds on a call).
-
-The most important thing to know about modules is that they are pluggable --
-this means that you only have to load modules that contain actions that you
-wish to use, which will make Jester run more effeciently.  You are encouraged
-to only load those modules that contain actions you use in the profile you're
-loading.
-
-One caveat to be aware of is that certain modules may depend on other modules
-being enabled for them to function properly.  Check the help for a module to
-see if it has dependencies on any other modules.
+--[[
+  Module help is stored with modules, and available at 'help modules'.  This
+  is merely a placeholder for the table of contents and help page summary.
 ]]
+jester.help_map.modules = {}
+jester.help_map.modules.description_short = [[View the list of available modules.  Run 'help module [name]' to get more help on a specific module.]]
+jester.help_map.modules["See 'help modules'"] = {}
+jester.help_map.modules["See 'help modules'"].description_short = [[]]
+
