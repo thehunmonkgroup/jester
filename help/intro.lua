8c5d244ebbc79e60c0cbee9cc17629d3f11a683b
Chad Phillips <chad@apartmentlines.com>
2010-12-18 18:18:57 -0700

    help sequences lua

--- help/intro.lua
+++ help/intro.lua
@@ -48,3 +48,83 @@ The help system lives in two distinct areas:
     These help files deal with most topics and are located at 'jester/help/*'
   2. Module help:
     Detailed help for modules and the actions they provide.  They are located in 'jester/modules/[name]/help.lua'.]]
+
+-- intro -> lua
+jester.help_map.intro.lua = {}
+jester.help_map.intro.lua.description_short = [[Brief Lua language tutorial.]]
+jester.help_map.intro.lua.description_long = [===[For a definitive explanation of the Lua language, the online Lua manual is the place to go:
+
+  http://www.lua.org/manual/5.1
+
+This will be a very quick overview of the basics that you'll most likely use in sequences.
+
+Lua is loosely typed, and will automatically convert types based on the operation you're trying to perform, eg. if you concatenate a string with an integer, the integer will be converted to a string.
+
+Define a variable:
+  name = value -- assign one variable to another
+  name = 0 -- assign the 'name' variable an integer value of zero.
+  name = "foo" -- assign the 'name' variable the string value of "foo"
+  name = [[foo]] -- assign the 'name' variable the string value of "foo"
+
+Math:
+  foo = foo + 1
+  foo = foo - 1
+  foo = foo * 1
+  foo = foo / 1
+
+Relational operators:
+  == -- equal
+  ~= -- not equal
+  <  -- less than
+  >  -- greater than
+  <= -- less than or equal to
+  >= -- greater than or equal to
+
+String concatenation:
+  newstring = oldstring .. "more"
+
+Conditional statements:
+  if foo ~= "yes" then
+    -- Do something
+  end
+  if foo == 1 then
+    -- Do some stuff
+  elseif foo == "bar" then
+    -- Do something else
+  else
+    -- Ok, do this
+  end
+
+Boolean is true or false.  Nil is the 'non-value'.  Comparison operators return false when what they compare is false or nil, all others return true.
+
+Logical operators:
+
+  foo = true and false -- returns false
+  foo = true and "bar" -- returns "bar"
+  foo = false or true -- returns true
+  foo = "foo" or "bar" returns "foo"
+  foo = false or nil -- returns nil
+  foo = true and "bar" or "baz" -- returns bar
+  foo = false and "bar" or "baz" -- returns baz
+
+Tables:
+  The only structured data in Lua.
+
+  foo = {} -- create an empty table.
+  table1 = {10, 20, 30} -- create an ordered list.
+  table2 = {
+    bar = "baz",
+    bang = "zoom",
+  } -- create a record-style table, this is not ordered!
+  value = table1[1] -- value is 10
+  value = table2.bar -- value is "baz"
+  complex = {
+    {
+      one = "two",
+    },
+    {
+      three = "four",
+    },
+  }
+  value = complex[1].one -- value is two]===]
+
