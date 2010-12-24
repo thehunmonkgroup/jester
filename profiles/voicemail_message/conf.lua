13ffd82dc762c79003758299ffb9efab18288348
Chad Phillips <chad@apartmentlines.com>
2010-12-24 13:42:12 -0700

    split voicemail profile into voicemail_message profile and voicemail_admin profile.

---
+++ profiles/voicemail_message/conf.lua
@@ -0,0 +1,49 @@
+--[[
+  Profile configuration file.  All variables put in here will be processed once
+  during the jester bootstrap.
+
+  If you have a variable foo that you want to have a value of "bar", do:
+  foo = "bar"
+
+  Array/record syntax is like this:
+  foo = { bar = "baz", bing = "bong" }
+
+  Variables from the main configuration may be used in values, by accessing
+  them through the jester.conf.<varname> namespace.
+
+  Channel variables may be used in values, by accessing them through the
+  jester._get_variable("<varname>") function.
+]]
+
+-- Do not edit or remove this line.
+module(..., package.seeall)
+
+-- Overrides the global debug configuration for this profile only.
+debug = true
+
+-- Modules to load.
+-- Overrides the global debug configuration for this profile only.
+modules = {
+  "core_actions",
+  "play",
+  "record",
+  "file",
+  "data",
+}
+-- Overrides the global debug configuration for this profile only.
+sequence_path = jester.conf.profile_path .. "/voicemail_message/sequences"
+
+voicemail_dir = jester.conf.base_dir .. "/storage/voicemail"
+temp_recording_dir = "/tmp"
+mailbox = jester.initial_args[1]
+context = jester.initial_args[2] or "default"
+domain = jester.get_variable("domain")
+
+mailbox_dir = voicemail_dir .. "/" .. context .. "/" .. domain .. "/" .. mailbox
+
+db_config_messages = {
+  database_type = "mysql",
+  database = "jester",
+  table = "messages",
+}
+
