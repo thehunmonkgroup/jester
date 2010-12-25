a920dac5b0e6b5d092a44908b30c3bb9a4ca3cf2
Chad Phillips <chad@apartmentlines.com>
2010-12-24 21:02:09 -0700

    bridge action.

--- modules/dialplan_tools/dialplan_tools.lua
+++ modules/dialplan_tools/dialplan_tools.lua
@@ -37,7 +37,90 @@ function bridge(action)
   local variables = action.variables or {}
   local multichannel_type = action.multichannel_type or "first_wins"
   local hangup_after_bridge = action.hangup_after_bridge
-  if channel and extension then
+  local data = ""
+  local global_vars = ""
+  local multivars, separator
+
+  -- A numeric key means it's multiple variable tables.
+  if variables[1] then
+    multivars = true
+  else
+    global_vars = build_variables(variables, "global")
   end
+  if channel and extension then
+    -- Save the current state of the variable so it can be restored.
+    local hangup_var = jester.get_variable("hangup_after_bridge", "true")
+    jester.set_variable("hangup_after_bridge", "false")
+    -- Complex dialstring.
+    if type(channel) == "table" or type(extension) == "table" then
+      local channel_pieces = {}
+      -- Multiple channels and one extension.
+      if type(channel) == "table" and type(extension) == "string" then
+        for k, chan in ipairs(channel) do
+          table.insert(channel_pieces, build_channel(variables, chan, extension, multivars, k))
+        end
+      -- Multiple extensions and one channel.
+      elseif type(channel) == "string" and type(extension) == "table" then
+        for k, exten in ipairs(extension) do
+          table.insert(channel_pieces, build_channel(variables, channel, exten, multivars, k))
+        end
+      -- Multiple extensions and channels.
+      else
+        for k, chan in ipairs(channel) do
+          table.insert(channel_pieces, build_channel(variables, chan, extension[k], multivars, k))
+        end
+      end
+      if multichannel_type == "first_wins" then
+        separator = ","
+      elseif multichannel_type == "sequential" then
+        separator = "|"
+      end
+      data = table.concat(channel_pieces, separator)
+    -- Simple dialsting.
+    else
+      data = channel .. extension
+    end
+    data = global_vars .. data
+    jester.debug_log("Bridging call to: %s", data)
+    session:execute("bridge", data)
+    -- Restore the original state of the variable.
+    jester.set_variable("hangup_after_bridge", hangup_var)
+    if hangup_after_bridge then
+      session:hangup()
+    end
+  end
+end
+
+--[[
+  Given a table of variables, return a string.
+]]
+function build_variables(variables, v_type)
+  local output = ""
+  local string_pieces = {}
+  for k, v in pairs(variables) do
+    table.insert(string_pieces, k .. "='" .. v .."'")
+  end
+  if #string_pieces > 0 then
+    output = table.concat(string_pieces, ",")
+    if v_type == "global" then
+      output = "{" .. output .. "}"
+    else
+      output = "[" .. output .. "]"
+    end
+  end
+  return output
+end
+
+--[[
+  Given a the main variables table and location data, a channel and extension,
+  return a fully constructed channel string for the channel.
+]]
+function build_channel(variables, channel, extension, multivars, k)
+  if multivars and variables[k] then
+    channel_vars = build_variables(variables[k])
+  else
+    channel_vars = ""
+  end
+  return channel_vars .. channel .. extension
 end
 
--- modules/dialplan_tools/help.lua
+++ modules/dialplan_tools/help.lua
@@ -27,8 +27,8 @@ jester.help_map.dialplan_tools.actions.bridge.description_long = [[This action b
 
 Note that many characteristics of the bridge can be controlled by setting various channel variables prior to the bridge.  Check the FreeSWITCH wiki for more information on the available channel variables, and 'help action set_variable' for setting channel variables from Jester.]]
 jester.help_map.dialplan_tools.actions.bridge.params = {
-  channel = [[The channel to use for the bridge.  Can be a string (used for all extensions), or optionally a table of multiple channels (used with the matching extension in the extension list).]],
-  extension = [[The extension to bridge to.  Can be a string (used for all channels), or optionally a table of multiple extensions (used with the matching channel in the channel list).]],
+  channel = [[The channel to use for the bridge.  Can be a string (used for all extensions), or optionally a table of multiple channels (used with the matching extension in the extension table).  Be sure to include everything up to the actual extension, including trailing slash and any dial prefix.]],
+  extension = [[The extension to bridge to.  Can be a string (used for all channels), or optionally a table of multiple extensions (used with the matching channel in the channel table).]],
   variables = [[A table of channel variables to set for the bridge, key = variable name, value = variable value.  Can be a single table (used for all channels), or optionally a table of variable tables (used with the matching channel in the channel list).]],
   multichannel_type = [[(Optional) If multiple channels or extensions are specified, this setting determines how they will be connected. 'first_wins' rings all channels until the first responds with media, then bridges that channel.  'sequential' rings each channel in succession, bridging the first one that responds with media.  Default is first_wins'.]],
   hangup_after_bridge = [[(Optional) If set to true, the call will be hungup after the bridge completes.  Note that all sequences registered for the exit and hangup loops will still be run.]],
