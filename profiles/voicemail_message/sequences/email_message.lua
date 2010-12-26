bf93372880519bbc17dec0c3b5681f793c719812
Chad Phillips <chad@apartmentlines.com>
2010-12-25 21:37:02 -0700

    voicemail to email sequence.

--- profiles/voicemail_message/sequences/email_message.lua
+++ profiles/voicemail_message/sequences/email_message.lua
@@ -1,5 +1,41 @@
+mailbox = args(1)
+caller_id_name = variable("caller_id_name")
+caller_id_number = variable("caller_id_number")
+recording_name = storage("record", "last_recording_name")
+timestamp = storage("record", "last_recording_timestamp")
+email = storage("mailbox_settings_message", "email")
+timzone = storage("mailbox_settings_message", "timezone")
+formatted_date = storage("format", "formatted_date")
+
 return
 {
-
+  {
+    action = "format_date",
+    storage_key = "formatted_date",
+    timestamp = timestamp,
+    timezone = timezone,
+  },
+  {
+    action = "email",
+    attachments = {
+      {
+        filetype = "audio/x-wav",
+        filename = "message.wav",
+        filepath = profile.temp_recording_dir .. "/" .. recording_name,
+      }
+    },
+    message = [[
+Mailbox number: :mailbox
+Date/time: :datetime
+CallerID number: :caller_id_number
+CallerID name: :caller_id_name]],
+    subject = "New voicemail message for :mailbox",
+    to = email,
+    tokens = {
+      mailbox = mailbox,
+      datetime = formatted_date,
+      caller_id_number = caller_id_number,
+      caller_id_name = caller_id_name,
+    },
+  },
 }
-
