90be722939eab2d6089dbd05954e2029813c9d12
Chad Phillips <chad@apartmentlines.com>
2011-01-01 00:47:35 -0700

    refactor save recording sequence to be more flexible.

--- profiles/voicemail/sequences/save_recorded_message.lua
+++ profiles/voicemail/sequences/save_recorded_message.lua
@@ -1,7 +1,14 @@
-recording_name = storage("record", "last_recording_name")
 mailbox = args(1)
 domain = args(2)
 operation = args(3)
+
+-- Message data.
+recording_name = storage("record", "last_recording_name")
+timestamp = storage("record", "last_recording_timestamp")
+duration = storage("record", "last_recording_duration")
+caller_id_number = storage("message_info", "caller_id_number")
+caller_id_name = storage("message_info", "caller_id_name")
+caller_domain = storage("message_info", "caller_domain")
 
 file_operation = {
   action = "move_file",
@@ -25,11 +32,11 @@ return
       mailbox = mailbox,
       -- Inbox folder is 0, all new messages go there.
       __folder = 0,
-      caller_id_number = variable("caller_id_number"),
-      caller_id_name = variable("caller_id_name"),
-      caller_domain = profile.caller_domain,
-      __timestamp = storage("record", "last_recording_timestamp"),
-      __duration = storage("record", "last_recording_duration"),
+      caller_id_number = caller_id_number,
+      caller_id_name = caller_id_name,
+      caller_domain = caller_domain,
+      __timestamp = timestamp,
+      __duration = duration,
       recording = recording_name,
     },
     update_type = "insert",
