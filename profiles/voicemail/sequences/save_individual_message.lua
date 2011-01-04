c4b7fd5008d9a5e6b4a19a1d9dca50f6a1b68742
Chad Phillips <chad@apartmentlines.com>
2011-01-03 17:51:19 -0700

    final cleanup in individual message save should be a subsequence. code comments.

--- profiles/voicemail/sequences/save_individual_message.lua
+++ profiles/voicemail/sequences/save_individual_message.lua
@@ -1,15 +1,26 @@
+--[[
+  Save a individual message.
+]]
+
+-- Mailbox info.
 mailbox = storage("message_info", "mailbox")
 domain = storage("message_info", "domain")
+
+-- Result of the attempt to load the mailbox.
 loaded_mailbox = storage("mailbox_settings_message", "mailbox")
+
+-- Mailbox settings.
 email_messages = storage("mailbox_settings_message", "email_messages")
 mailbox_provisioned = storage("mailbox_settings_message", "mailbox_provisioned")
 
 return
 {
+  -- Try to load the mailbox.
   {
     action = "call_sequence",
     sequence = "sub:load_mailbox_settings " .. mailbox .. "," .. domain .. ",mailbox_settings_message",
   },
+  -- Mailbox load failed, clean up.
   {
     action = "conditional",
     value = loaded_mailbox,
@@ -17,6 +28,7 @@ return
     comparison = "equal",
     if_true = "cleanup_temp_recording",
   },
+  -- Provision the mailbox if it's not provisioned.
   {
     action = "conditional",
     value = mailbox_provisioned,
@@ -24,6 +36,7 @@ return
     comparison = "equal",
     if_true = "sub:provision_mailbox " .. mailbox .. "," .. domain,
   },
+  -- Email the message if necessary.
   {
     action = "conditional",
     value = email_messages,
@@ -31,12 +44,13 @@ return
     comparison = "equal",
     if_false = "sub:email_message",
   },
+  -- Save the message to the mailbox if necessary, otherwise clean up.
   {
     action = "conditional",
     value = email_messages,
     compare_to = "email_only",
     comparison = "equal",
-    if_true = "cleanup_temp_recording",
+    if_true = "sub:cleanup_temp_recording",
     if_false = "sub:save_recorded_message",
   },
 }
