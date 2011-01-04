69767704cd99d7aa7179757aa57a7d9527fb1996
Chad Phillips <chad@apartmentlines.com>
2011-01-03 23:39:32 -0700

    ensure session before checking for ready state.

--- core.lua
+++ core.lua
@@ -575,7 +575,9 @@ end
   you want to loop until the call hangs up.
 ]]
 function ready()
-  return session:ready() or exiting or hungup
+  if session then
+    return session:ready() or exiting or hungup
+  end
 end
 
 --[[
