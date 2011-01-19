65cadaf6e6cba1e2962b7a5af31311bc60d29059
Chad Phillips <chad@apartmentlines.com>
2011-01-19 12:19:20 -0700

    clean up help doc for record action.

--- modules/record/help.lua
+++ modules/record/help.lua
@@ -24,13 +24,13 @@ The following variables/values related to the recording are put into Jester stor
 jester.help_map.record.actions.record.params = {
   filename = [[(Optional) The name of the recorded file.  Defaults to %Y-%m-%d_%H:%M:%S-${uuid}.wav]],
   location = [[(Optional) Where to store the file.  Default is /tmp.]],
-  append = [[(Optional) Append the recording to an existing file.  Requires that the 'name' parameter be set.  If the named file does not exist, then it will be created.  Default is false.]],
+  append = [[(Optional) Append the recording to an existing file.  Requires that the 'filename' parameter be set.  If the named file does not exist, then it will be created.  Default is false.]],
   pre_record_sound = [[(Optional) Set to a file or phrase to play prior to beginning the recording, or to 'tone' to play a typical 'wait for the beep' tone.  Default is to do nothing.]],
   pre_record_delay = [[(Optional) Set to the number of milliseconds to delay just prior to beginning the recording.  This happens after the pre_record_sound is played.  This can be useful to tweak if trailing channel sounds are being recording at the beginning of the recording.  Set to 0 for no delay.  Default is 200 milliseconds.]],
   max_length = [[(Optional) Maximum allowed length of the recording in seconds.  Default is 180.]],
   silence_threshold = [[(Optional) A number indicating the threshhold for what is considered silence.  Higher numbers mean more noise will be tolerated.  Default is 20.  TODO: need to find doc on this.]],
   silence_secs = [[(Optional) The number of consecutive seconds of silence to wait before considering the recording finished.  Default is 5.]],
-  storage_area = [[(Optional) If set the 'last_recording' storage values are also stored this storage area with the 'last_recording_' prefix stripped, eg. 'storage_area = "message"' would store 'name' in the 'message' storage area with the same value as 'last_recording_name'.]],
+  storage_area = [[(Optional) If set the 'last_recording' storage values are also stored in this storage area with the 'last_recording_' prefix stripped, eg. 'storage_area = "message"' would store 'name' in the 'message' storage area with the same value as 'last_recording_name'.]],
   keys = [[(Optional) See 'help sequences keys'.]],
 }
 
