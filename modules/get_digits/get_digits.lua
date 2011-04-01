0d1cfb9d51378a0d7038aa3ed2499fa186d5da78
Chad Phillips <chad@apartmentlines.com>
2011-04-01 00:57:13 -0600

    refactor custom setting of min/max digit length in get_digits action -- now either can be set to the length of a string by passing it to the parameter prefixed with a colon.

--- modules/get_digits/get_digits.lua
+++ modules/get_digits/get_digits.lua
@@ -4,8 +4,18 @@ module(..., package.seeall)
   Gets digit input from the user.
 ]]
 function get_digits(action)
-  local min_digits = action.min_digits or 1
-  local max_digits = action.max_digits or 10
+  local min_digits = action.min_digits
+  local max_digits = action.max_digits
+  if min_digits then
+    min_digits = set_digit_length(min_digits)
+  else
+    min_digits = 1
+  end
+  if max_digits then
+    max_digits = set_digit_length(max_digits)
+  else
+    max_digits = 10
+  end
   local max_tries = action.max_tries or 3
   local timeout = action.timeout and (action.timeout * 1000) or 3000
   local terminators = action.terminators or "#"
@@ -16,18 +26,20 @@ function get_digits(action)
   end
   local bad_input = action.bad_input or "ivr/ivr-that_was_an_invalid_entry.wav"
   local digits_regex = action.digits_regex or "\\d+"
-  local exact_match = action.exact_match
   local key = action.storage_key or "digits"
-  -- Since exact match was passed, we can use it's length to set min and max
-  -- digits, and save the user any timeout wait.
-  if exact_match then
-    local digit_length = string.len(exact_match)
-    min_digits = digit_length
-    max_digits = digit_length
-    digits_regex = "^" .. exact_match .. "$"
-  end
   local digits = session:playAndGetDigits(min_digits, max_digits, max_tries, timeout, terminators, audio_files, bad_input, digits_regex)
   jester.debug_log("Got digits: %s", digits)
   jester.set_storage("get_digits", key, digits)
+end
+
+--[[
+  Set the digit length to the length of the passed string if prefixed with the
+  length operator.
+]]
+function set_digit_length(digit_length)
+  if type(digit_length) == "string" and string.sub(digit_length, 1, 1) == ":" then
+    digit_length = string.len(digit_length) - 1
+  end
+  return digit_length
 end
 
