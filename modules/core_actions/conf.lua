68bcdd8c2d7df331563aa05082042416c20993f8
Chad Phillips <chad@apartmentlines.com>
2016-01-09 22:03:41 -0800

    lua 5.2 port of core actions module.

--- modules/core_actions/conf.lua
+++ modules/core_actions/conf.lua
@@ -1,45 +1,51 @@
-jester.action_map.none = {
-  mod = "core_actions",
-  func = "none",
+local conf = {
+  action_map = {
+    none = {
+      mod = "core_actions",
+      func = "none",
+    }
+    call_sequence = {
+      mod = "core_actions",
+      func = "call_sequence",
+    }
+    conditional = {
+      mod = "core_actions",
+      func = "conditional",
+    }
+    set_variable = {
+      mod = "core_actions",
+      func = "set_variable",
+    }
+    set_storage = {
+      mod = "core_actions",
+      func = "set_storage",
+    }
+    copy_storage = {
+      mod = "core_actions",
+      func = "copy_storage",
+    }
+    clear_storage = {
+      mod = "core_actions",
+      func = "clear_storage",
+    }
+    exit_sequence = {
+      mod = "core_actions",
+      func = "register_exit_sequence",
+    }
+    wait = {
+      mod = "core_actions",
+      func = "wait",
+    }
+    load_profile = {
+      mod = "core_actions",
+      func = "load_profile",
+    }
+    api_command = {
+      mod = "core_actions",
+      func = "api_command",
+    }
+  }
 }
-jester.action_map.call_sequence = {
-  mod = "core_actions",
-  func = "call_sequence",
-}
-jester.action_map.conditional = {
-  mod = "core_actions",
-  func = "conditional",
-}
-jester.action_map.set_variable = {
-  mod = "core_actions",
-  func = "set_variable",
-}
-jester.action_map.set_storage = {
-  mod = "core_actions",
-  func = "set_storage",
-}
-jester.action_map.copy_storage = {
-  mod = "core_actions",
-  func = "copy_storage",
-}
-jester.action_map.clear_storage = {
-  mod = "core_actions",
-  func = "clear_storage",
-}
-jester.action_map.exit_sequence = {
-  mod = "core_actions",
-  func = "register_exit_sequence",
-}
-jester.action_map.wait = {
-  mod = "core_actions",
-  func = "wait",
-}
-jester.action_map.load_profile = {
-  mod = "core_actions",
-  func = "load_profile",
-}
-jester.action_map.api_command = {
-  mod = "core_actions",
-  func = "api_command",
-}
+
+return conf
 
--- modules/core_actions/core_actions.lua
+++ modules/core_actions/core_actions.lua
@@ -1,30 +1,32 @@
-module(..., package.seeall)
+local core = require "jester.core"
+
+local _M = {}
 
 --[[
   Dummy action.
 ]]
-function none(action) end
+function _M.none(action) end
 
 --[[
   Call a sequence.
 ]]
-function call_sequence(action)
+function _M.call_sequence(action)
   if action.sequence then
-    jester.queue_sequence(action.sequence)
+    core.queue_sequence(action.sequence)
   end
 end
 
 --[[
   Compare two values, and optionally call sequences if they match or not.
 ]]
-function conditional(action)
+function _M.conditional(action)
   local value = action.value
   local compare_to = action.compare_to
   local operator = action.comparison or "equal"
   local if_true = action.if_true
   local if_false = action.if_false
   if value and compare_to then
-    jester.debug_log("Comparing '%s' to '%s' using comparison method '%s'", tostring(value), tostring(compare_to), operator)
+    core.debug_log("Comparing '%s' to '%s' using comparison method '%s'", tostring(value), tostring(compare_to), operator)
     local match
     if operator == "equal" then
       match = value == compare_to
@@ -36,16 +38,16 @@ function conditional(action)
       match = value < compare_to
     end
     if match == nil then
-      jester.debug_log("Invalid comparison operator")
+      core.debug_log("Invalid comparison operator")
     elseif match == false then
-      jester.debug_log("Comparison result: false")
+      core.debug_log("Comparison result: false")
       if if_false then
-        jester.queue_sequence(if_false)
+        core.queue_sequence(if_false)
       end
     else
-      jester.debug_log("Comparison result: true")
+      core.debug_log("Comparison result: true")
       if if_true then
-        jester.queue_sequence(if_true)
+        core.queue_sequence(if_true)
       end
     end
   end
@@ -54,43 +56,43 @@ end
 --[[
   Set channel variables.
 ]]
-function set_variable(action)
+function _M.set_variable(action)
   local data = action.data or {}
   for k, v in pairs(data) do
-    jester.set_variable(k, v)
+    core.set_variable(k, v)
   end
 end
 
 --[[
   Set storage variables.
 ]]
-function set_storage(action)
+function _M.set_storage(action)
   local area = action.storage_area or "default"
   local data = action.data or {}
   for k, v in pairs(data) do
-    jester.set_storage(area, k, v)
+    core.set_storage(area, k, v)
   end
 end
 
 --[[
   Copy storage from one area to another.
 ]]
-function copy_storage(action)
+function _M.copy_storage(action)
   local area = action.storage_area or "default"
   local copy_area = action.copy_to
   local move = action.move
   if copy_area then
     -- Clear out the copy area before copying.
-    jester.clear_storage(copy_area)
-    jester.debug_log("Copying data from area '%s' to area '%s'", area, copy_area)
-    for k, v in pairs(jester.channel.storage[area]) do
+    core.clear_storage(copy_area)
+    core.debug_log("Copying data from area '%s' to area '%s'", area, copy_area)
+    for k, v in pairs(core.channel.storage[area]) do
       -- Only copy basic data types, others will not copy reliably.
       if type(v) == "string" or type(v) == "number" or type(v) == "boolean" then
-        jester.set_storage(copy_area, k, v)
+        core.set_storage(copy_area, k, v)
       end
     end
     if move then
-      jester.clear_storage(area)
+      core.clear_storage(area)
     end
   end
 end
@@ -98,61 +100,61 @@ end
 --[[
   Clear a storage key or storage area.
 ]]
-function clear_storage(action)
+function _M.clear_storage(action)
   local area = action.storage_area or "default"
   local data_keys = action.data_keys
   if data_keys then
     if type(data_keys) == "table" then
       for _, v in ipairs(data_keys) do
-        jester.clear_storage(area, v)
+        core.clear_storage(area, v)
       end
     end
   else
-    jester.clear_storage(area)
+    core.clear_storage(area)
   end
 end
 
 --[[
   Register a sequence to run in the exit sequence loop.
 ]]
-function register_exit_sequence(action)
+function _M.register_exit_sequence(action)
   local sequence = action.sequence
   if sequence then
     local event = {}
     event.event_type = "sequence"
     event.sequence = sequence
-    table.insert(jester.channel.stack.exit, event)
-    jester.debug_log("Registered exit sequence: %s", sequence)
+    table.insert(core.channel.stack.exit, event)
+    core.debug_log("Registered exit sequence: %s", sequence)
   end
 end
 
 --[[
   Wait for a specified amount of time.
 ]]
-function wait(action)
+function _M.wait(action)
   local milliseconds = action.milliseconds
   if milliseconds then
-    jester.wait(milliseconds)
+    core.wait(milliseconds)
   end
 end
 
 --[[
   Load another profile.
 ]]
-function load_profile(action)
+function _M.load_profile(action)
   local profile = action.profile
   local sequence = action.sequence
   if profile then
     -- Copy in new initial args here if a sequence is going to be called, so
     -- profile has access to them.
     if sequence then
-      local s_type, sequence_name, sequence_args = jester.parse_sequence(sequence)
-      jester.initial_args = jester.parse_args(sequence_args)
+      local s_type, sequence_name, sequence_args = core.parse_sequence(sequence)
+      core.initial_args = core.parse_args(sequence_args)
     end
-    jester.init_profile(profile)
-    jester.init_modules(jester.conf.modules)
+    core.init_profile(profile)
+    core.init_modules(core.conf.modules)
     if sequence then
-      jester.queue_sequence(sequence)
+      core.queue_sequence(sequence)
     end
   end
 end
@@ -160,17 +162,18 @@ end
 --[[
   Execute a FreeSWITCH API command.
 ]]
-function api_command(action)
+function _M.api_command(action)
   local command = action.command
   local key = action.storage_key or "result"
   if command then
-    jester.debug_log("Executing API command: %s", command)
+    core.debug_log("Executing API command: %s", command)
     local api = freeswitch.API()
     local result = api:executeString(command)
     -- Remove final carriage return from result.
     result = string.sub(result, 1, -2)
-    jester.debug_log("Command result: %s", result)
-    jester.set_storage("api_command", key, result)
+    core.debug_log("Command result: %s", result)
+    core.set_storage("api_command", key, result)
   end
 end
 
+return _M
