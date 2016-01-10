44395e9dcaccddf37baad17fe7b720fd1da6e723
Chad Phillips <chad@apartmentlines.com>
2016-01-10 09:24:17 -0800

    lua 5.2 port for file module.

--- modules/file/conf.lua
+++ modules/file/conf.lua
@@ -1,43 +1,55 @@
-jester.action_map.create_directory = {
-  mod = "file",
-  handlers = {
-    filesystem = "create_directory",
-    default = "create_directory",
-  }
-}
-jester.action_map.remove_directory = {
-  mod = "file",
-  handlers = {
-    filesystem = "remove_directory",
-    default = "remove_directory",
-  }
-}
-jester.action_map.move_file = {
-  mod = "file",
-  handlers = {
-    filesystem = "move_file",
-    default = "move_file",
-  }
-}
-jester.action_map.delete_file = {
-  mod = "file",
-  handlers = {
-    filesystem = "delete_file",
-    default = "delete_file",
-  }
-}
-jester.action_map.file_exists = {
-  mod = "file",
-  handlers = {
-    filesystem = "file_exists",
-    default = "file_exists",
-  }
-}
-jester.action_map.file_size = {
-  mod = "file",
-  handlers = {
-    filesystem = "file_size",
-    default = "file_size",
+local conf = {
+  action_map = {
+
+    create_directory = {
+      mod = "file",
+      handlers = {
+        filesystem = "create_directory",
+        default = "create_directory",
+      }
+    },
+
+    remove_directory = {
+      mod = "file",
+      handlers = {
+        filesystem = "remove_directory",
+        default = "remove_directory",
+      }
+    },
+
+    move_file = {
+      mod = "file",
+      handlers = {
+        filesystem = "move_file",
+        default = "move_file",
+      }
+    },
+
+    delete_file = {
+      mod = "file",
+      handlers = {
+        filesystem = "delete_file",
+        default = "delete_file",
+      }
+    },
+
+    file_exists = {
+      mod = "file",
+      handlers = {
+        filesystem = "file_exists",
+        default = "file_exists",
+      }
+    },
+
+    file_size = {
+      mod = "file",
+      handlers = {
+        filesystem = "file_size",
+        default = "file_size",
+      }
+    },
+
   }
 }
 
+return conf
--- modules/file/file.lua
+++ modules/file/file.lua
@@ -1,12 +1,14 @@
-module(..., package.seeall)
+local core = require "jester.core"
+
+local _M = {}
 
 --[[
   Appends the freeswitch base_dir if a relative path is passed.
 ]]
-function get_filepath(file)
+local function get_filepath(file)
   -- Look for leading slash to indicate full path.
   if file:sub(1, 1) ~= "/" then
-    file = jester.conf.base_dir .. "/" .. file
+    file = core.conf.base_dir .. "/" .. file
   end
   return file
 end
@@ -14,7 +16,7 @@ end
 --[[
   Create a directory.
 ]]
-function create_directory(action)
+function _M.create_directory(action)
   local directory = action.directory
   if directory then
     local success, file_error
@@ -23,25 +25,25 @@ function create_directory(action)
     -- Look for existing directory.
     success, file_error = lfs.attributes(path, "mode")
     if success then
-      jester.debug_log("Directory '%s' already exists, skipping creation.", path)
+      core.debug_log("Directory '%s' already exists, skipping creation.", path)
     else
       -- Create new directory.
       success, file_error = lfs.mkdir(path)
       if success then
-        jester.debug_log("Created directory '%s'", path)
+        core.debug_log("Created directory '%s'", path)
       else
-        jester.debug_log("Failed to create directory '%s'!: %s", path, file_error)
+        core.debug_log("Failed to create directory '%s'!: %s", path, file_error)
       end
     end
   else
-    jester.debug_log("Cannot create directory, no 'path' parameter defined!")
+    core.debug_log("Cannot create directory, no 'path' parameter defined!")
   end
 end
 
 --[[
   Remove a directory.
 ]]
-function remove_directory(action)
+function _M.remove_directory(action)
   local directory = action.directory
   if directory then
     local success, file_error
@@ -53,22 +55,22 @@ function remove_directory(action)
       -- Remove directory.
       success, file_error = lfs.rmdir(path)
       if success then
-        jester.debug_log("Deleted directory '%s'", path)
+        core.debug_log("Deleted directory '%s'", path)
       else
-        jester.debug_log("Failed to delete directory '%s'!: %s", path, file_error)
+        core.debug_log("Failed to delete directory '%s'!: %s", path, file_error)
       end
     else
-      jester.debug_log("Directory '%s' does not exist, skipping removal.", path)
+      core.debug_log("Directory '%s' does not exist, skipping removal.", path)
     end
   else
-    jester.debug_log("Cannot delete directory, no 'path' parameter defined!")
+    core.debug_log("Cannot delete directory, no 'path' parameter defined!")
   end
 end
 
 --[[
   Move or copy a file.
 ]]
-function move_file(action)
+function _M.move_file(action)
   local operation = action.copy and "copy" or "move"
   local binary = action.binary and "b" or ""
   if action.source and action.destination then
@@ -93,36 +95,36 @@ function move_file(action)
       end
     end
     if success then
-      jester.debug_log("Successful file %s from '%s' to '%s'", operation, source_file, destination_file)
+      core.debug_log("Successful file %s from '%s' to '%s'", operation, source_file, destination_file)
     else
-      jester.debug_log("Failed file %s from '%s' to '%s'!: %s", operation, source_file, destination_file, file_error)
+      core.debug_log("Failed file %s from '%s' to '%s'!: %s", operation, source_file, destination_file, file_error)
     end
   else
-    jester.debug_log("Cannot perform file %s, missing parameter! Source: %s, Destination: %s", operation, tostring(action.source), tostring(action.destination))
+    core.debug_log("Cannot perform file %s, missing parameter! Source: %s, Destination: %s", operation, tostring(action.source), tostring(action.destination))
   end
 end
 
 --[[
   Delete a file.
 ]]
-function delete_file(action)
+function _M.delete_file(action)
   if action.file then
     local file = get_filepath(action.file)
     local success, file_error = os.remove(file)
     if success then
-      jester.debug_log("Deleted file '%s'", file)
+      core.debug_log("Deleted file '%s'", file)
     else
-      jester.debug_log("Failed to delete file '%s'!: %s", file, file_error)
+      core.debug_log("Failed to delete file '%s'!: %s", file, file_error)
     end
   else
-    jester.debug_log("Cannot delete file, no 'file' parameter defined!")
+    core.debug_log("Cannot delete file, no 'file' parameter defined!")
   end
 end
 
 --[[
   Check for file existence.
 ]]
-function file_exists(action)
+function _M.file_exists(action)
   local result, file
   if action.file then
     require "lfs"
@@ -138,28 +140,28 @@ function file_exists(action)
     result = ""
   end
   -- Store the result of the check.
-  jester.set_storage("file", "file_exists", result)
+  core.set_storage("file", "file_exists", result)
   if result == "false" then
-    jester.debug_log("File '%s' does not exist", file)
+    core.debug_log("File '%s' does not exist", file)
     -- Run false sequence if specified.
     if action.if_false then
-      jester.queue_sequence(action.if_false)
+      core.queue_sequence(action.if_false)
     end
   elseif result == "true" then
-    jester.debug_log("File '%s' exists", file)
+    core.debug_log("File '%s' exists", file)
     -- Run true sequence if specified.
     if action.if_true then
-      jester.queue_sequence(action.if_true)
+      core.queue_sequence(action.if_true)
     end
   else
-    jester.debug_log("Cannot check file, no 'file' parameter defined!")
+    core.debug_log("Cannot check file, no 'file' parameter defined!")
   end
 end
 
 --[[
   Returns a file's size.
 ]]
-function file_size(action)
+function _M.file_size(action)
   local file, size
   if action.file then
     require "lfs"
@@ -167,5 +169,7 @@ function file_size(action)
     size = lfs.attributes(file, "size")
   end
   -- Store the result of the check.
-  jester.set_storage("file", "size", size)
+  core.set_storage("file", "size", size)
 end
+
+return _M
