1613f213aaf95c4c6bcedad831e8054d17fa2d66
Chad Phillips <chad@apartmentlines.com>
2016-01-09 22:07:39 -0800

    fix  missing commas.

--- modules/core_actions/conf.lua
+++ modules/core_actions/conf.lua
@@ -1,49 +1,61 @@
 local conf = {
   action_map = {
+
     none = {
       mod = "core_actions",
       func = "none",
-    }
+    },
+
     call_sequence = {
       mod = "core_actions",
       func = "call_sequence",
-    }
+    },
+
     conditional = {
       mod = "core_actions",
       func = "conditional",
-    }
+    },
+
     set_variable = {
       mod = "core_actions",
       func = "set_variable",
-    }
+    },
+
     set_storage = {
       mod = "core_actions",
       func = "set_storage",
-    }
+    },
+
     copy_storage = {
       mod = "core_actions",
       func = "copy_storage",
-    }
+    },
+
     clear_storage = {
       mod = "core_actions",
       func = "clear_storage",
-    }
+    },
+
     exit_sequence = {
       mod = "core_actions",
       func = "register_exit_sequence",
-    }
+    },
+
     wait = {
       mod = "core_actions",
       func = "wait",
-    }
+    },
+
     load_profile = {
       mod = "core_actions",
       func = "load_profile",
-    }
+    },
+
     api_command = {
       mod = "core_actions",
       func = "api_command",
-    }
+    },
+
   }
 }
 
