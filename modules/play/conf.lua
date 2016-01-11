9e4ed2540da8c597b1a2d3ddf2d114df8a3718dd
Chad Phillips <chad@apartmentlines.com>
2016-01-10 17:27:18 -0800

    lua 5.2 port for play module.

--- modules/play/conf.lua
+++ modules/play/conf.lua
@@ -1,22 +1,33 @@
-jester.action_map.play = {
-  mod = "play",
-  handlers = {
-    file = "play_file",
-    default = "play_file",
+local conf = {
+  action_map = {
+
+    play = {
+      mod = "play",
+      handlers = {
+        file = "play_file",
+        default = "play_file",
+      }
+    },
+
+    play_valid_file = {
+      mod = "play",
+      handlers = {
+        file = "play_valid_file",
+        default = "play_valid_file",
+      }
+    },
+
+    play_phrase = {
+      mod = "play",
+      func = "play_phrase_macro"
+    },
+
+    play_keys = {
+      mod = "play",
+      func = "play_key_macros"
+    },
+
   }
 }
-jester.action_map.play_valid_file = {
-  mod = "play",
-  handlers = {
-    file = "play_valid_file",
-    default = "play_valid_file",
-  }
-}
-jester.action_map.play_phrase = {
-  mod = "play",
-  func = "play_phrase_macro"
-}
-jester.action_map.play_keys = {
-  mod = "play",
-  func = "play_key_macros"
-}
+
+return conf
--- modules/play/play.lua
+++ modules/play/play.lua
@@ -1,40 +1,42 @@
-module(..., package.seeall)
+local core = require "jester.core"
+
+local _M = {}
 
 --[[
   Wait a specified time.
 ]]
-function wait(action)
+local function wait(action)
   -- Don't wait if the call is hung up, or if a key was pressed.
-  if action.wait and not jester.actionable_key() and jester.ready() then
-    jester.wait(action.wait)
+  if action.wait and not core.actionable_key() and core.ready() then
+    core.wait(action.wait)
   end
 end
 
 --[[
   Calculate the number of repetitions.
 ]]
-function reps(action)
+local function reps(action)
   return action.repetitions and tonumber(action.repetitions) or 1
 end
 
 --[[
   Play one or more files.
 ]]
-function play_file(action)
+function _M.play_file(action)
   if action.file then
     -- A list of files was passed, join them in order.
     if type(action.file) == "table" then
-      local delimiter = jester.conf.playback_delimiter or "&"
+      local delimiter = core.conf.playback_delimiter or "&"
       -- Ensure this channel variable is properly set, or playback of multiple
       -- files will not work.
-      jester.set_variable("playback_delimiter", delimiter)
+      core.set_variable("playback_delimiter", delimiter)
       action.file = table.concat(action.file, delimiter)
     end
     local rep = reps(action)
     for i = 1, rep do
-      jester.debug_log("Streaming file '%s'", action.file)
+      core.debug_log("Streaming file '%s'", action.file)
       -- Break out of the loop if an actionable key was pressed.
-      if not jester.ready() or jester.actionable_key() then return end
+      if not core.ready() or core.actionable_key() then return end
       session:streamFile(action.file)
       wait(action)
     end
@@ -44,7 +46,7 @@ end
 --[[
   Play the first valid file in a list of files.
 ]]
-function play_valid_file(action)
+function _M.play_valid_file(action)
   local files = action.files
   if type(files) == "table" then
     require "lfs"
@@ -57,7 +59,7 @@ function play_valid_file(action)
           success = true
         -- Relative path, appends the sounds_dir value.
         else
-          filepath = jester.conf.sounds_dir .. "/" .. file
+          filepath = core.conf.sounds_dir .. "/" .. file
         end
       else
         -- Absolute path.
@@ -69,31 +71,31 @@ function play_valid_file(action)
       end
       -- Play the file if it exists.
       if success then
-        jester.debug_log("Found valid file to play: %s", file)
+        core.debug_log("Found valid file to play: %s", file)
         -- Store the name of the valid file found.
-        jester.set_storage("play", "valid_file_played", file)
+        core.set_storage("play", "valid_file_played", file)
         action.file = file
-        return play_file(action)
+        return _M.play_file(action)
       end
     end
   end
   -- No valid file found, make sure to clear out this value from any previous
   -- attempts.
-  jester.debug_log("No valid file found to play.")
-  jester.set_storage("play", "valid_file_played", "")
+  core.debug_log("No valid file found to play.")
+  core.set_storage("play", "valid_file_played", "")
 end
 
 --[[
   Play a phrase macro.
 ]]
-function play_phrase_macro(action)
+function _M.play_phrase_macro(action)
   if action.phrase then
     local phrase_arguments = action.phrase_arguments or ""
     local rep = reps(action)
     for i = 1, rep do
-      jester.debug_log("Playing phrase '%s' with arguments '%s', language: %s", action.phrase, phrase_arguments, tostring(action.language))
+      core.debug_log("Playing phrase '%s' with arguments '%s', language: %s", action.phrase, phrase_arguments, tostring(action.language))
       -- Break out of the loop if an actionable key was pressed.
-      if not jester.ready() or jester.actionable_key() then return end
+      if not core.ready() or core.actionable_key() then return end
       if action.language then
         session:sayPhrase(action.phrase, phrase_arguments, action.language)
       else
@@ -107,20 +109,20 @@ end
 --[[
   Play a series of phrase macros that are mapped to keys.
 ]]
-function play_key_macros(action)
+function _M.play_key_macros(action)
   local macros = action.key_announcements
   if macros then
     -- Allow the action to override the default key order.
-    local order = action.order or jester.conf.key_order
+    local order = action.order or core.conf.key_order
     local language = action.language or "en"
     local rep = reps(action)
     for i = 1, rep do
-      jester.debug_log("Announcing keys, repitition %d", i)
+      core.debug_log("Announcing keys, repitition %d", i)
       for _, key in ipairs(order) do
         -- Break out of the loop if an actionable key was pressed.
-        if not jester.ready() or jester.actionable_key() then return end
+        if not core.ready() or core.actionable_key() then return end
         if macros[key] then
-          jester.debug_log("Playing key phrase '%s' with key '%s'", macros[key], key)
+          core.debug_log("Playing key phrase '%s' with key '%s'", macros[key], key)
           session:sayPhrase(macros[key], key, language)
         end
       end
