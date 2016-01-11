0fd6aff503726d69272c22079f96fb10683ae67b
Chad Phillips <chad@apartmentlines.com>
2016-01-10 16:11:05 -0800

    lua 5.2 port for navigation module.

--- modules/navigation/conf.lua
+++ modules/navigation/conf.lua
@@ -1,21 +1,32 @@
-jester.action_map.add_to_stack = {
-  mod = "navigation",
-  func = "add_to_stack",
-}
-jester.action_map.navigation_up = {
-  mod = "navigation",
-  func = "navigation_up",
-}
-jester.action_map.navigation_clear = {
-  mod = "navigation",
-  func = "navigation_clear",
-}
-jester.action_map.navigation_top = {
-  mod = "navigation",
-  func = "navigation_top",
-}
-jester.action_map.navigation_reset = {
-  mod = "navigation",
-  func = "navigation_reset",
+local conf = {
+  action_map = {
+
+    add_to_stack = {
+      mod = "navigation",
+      func = "add_to_stack",
+    },
+
+    navigation_up = {
+      mod = "navigation",
+      func = "navigation_up",
+    },
+
+    navigation_clear = {
+      mod = "navigation",
+      func = "navigation_clear",
+    },
+
+    navigation_top = {
+      mod = "navigation",
+      func = "navigation_top",
+    },
+
+    navigation_reset = {
+      mod = "navigation",
+      func = "navigation_reset",
+    },
+
+  }
 }
 
+return conf
--- modules/navigation/navigation.lua
+++ modules/navigation/navigation.lua
@@ -1,11 +1,13 @@
-module(..., package.seeall)
+local core = require "jester.core"
+
+local _M = {}
 
 --[[
   Initialize the navigation module.
 ]]
-function init()
+local function init()
   -- Initialize navigation stack.
-  jester.reset_stack("navigation")
+  core.reset_stack("navigation")
 
   -- Register clearing the stack on hangup.
   local event = {}
@@ -16,29 +18,29 @@ function init()
   }
   -- We want this to be cleared before any user hangup sequences/actions are
   -- run, so force it into first position.
-  table.insert(jester.channel.stack.exit, 1, event)
+  table.insert(core.channel.stack.exit, 1, event)
 end
 
 --[[
   Log the current navigation stack.
 ]]
-function show_navigation_stack(stack)
-  jester.debug_log("Current navigation stack: %s", table.concat(stack, " | "))
+local function show_navigation_stack(stack)
+  core.debug_log("Current navigation stack: %s", table.concat(stack, " | "))
 end
 
 --[[
   Add a sequence to the navigation stack.
 ]]
-function add_to_stack(action)
-  local stack = jester.channel.stack.navigation
-  local sequence_stack = jester.channel.stack.sequence
-  local p = jester.channel.stack.sequence_stack_position
+function _M.add_to_stack(action)
+  local stack = core.channel.stack.navigation
+  local sequence_stack = core.channel.stack.sequence
+  local p = core.channel.stack.sequence_stack_position
   -- Default to the currently running sequence.
   local sequence = action.sequence or (sequence_stack[p].name .. " " .. sequence_stack[p].args)
   -- Don't add to the stack if the last sequence on the stack is the same.
   if not (sequence == stack[#stack]) then
-    table.insert(jester.channel.stack.navigation, sequence)
-    jester.debug_log("Adding '%s' to navigation stack", sequence)
+    table.insert(core.channel.stack.navigation, sequence)
+    core.debug_log("Adding '%s' to navigation stack", sequence)
     show_navigation_stack(stack)
   end
 end
@@ -46,68 +48,69 @@ end
 --[[
   Go up the navigation stack one level.
 ]]
-function navigation_up(action)
-  local stack = jester.channel.stack.navigation
+function _M.navigation_up(action)
+  local stack = core.channel.stack.navigation
   if #stack == 0 then
-    jester.debug_log("Cannnot navigate up the stack, stack is empty!")
+    core.debug_log("Cannnot navigate up the stack, stack is empty!")
   else
     local last_sequence, new_sequence
     -- Remove the current sequence from the stack unless it's the only one.
     if #stack > 1 then
-      last_sequence = table.remove(jester.channel.stack.navigation)
+      last_sequence = table.remove(core.channel.stack.navigation)
     end
     -- Last item on the stack is now up one level.
     new_sequence = stack[#stack]
-    jester.debug_log("Moving up the stack from sequence '%s' to sequence '%s'", tostring(last_sequence), new_sequence)
+    core.debug_log("Moving up the stack from sequence '%s' to sequence '%s'", tostring(last_sequence), new_sequence)
     show_navigation_stack(stack)
-    jester.queue_sequence(new_sequence)
+    core.queue_sequence(new_sequence)
   end
 end
 
 --[[
   Clear the navigation stack.
 ]]
-function navigation_clear(action)
-  jester.channel.stack.navigation = {}
-  jester.debug_log("Navigation stack cleared.")
+function _M.navigation_clear(action)
+  core.channel.stack.navigation = {}
+  core.debug_log("Navigation stack cleared.")
 end
 
 --[[
   Go to the top of the navigation stack.
 ]]
-function navigation_top(action)
-  local stack = jester.channel.stack.navigation
+function _M.navigation_top(action)
+  local stack = core.channel.stack.navigation
   if #stack == 0 then
-    jester.debug_log("Cannnot navigate to the top of the stack, stack is empty!")
+    core.debug_log("Cannnot navigate to the top of the stack, stack is empty!")
   else
     local last_sequence, new_sequence
     last_sequence = stack[#stack]
     new_sequence = stack[1]
     -- New stack starts with first sequence from old stack.
-    jester.channel.stack.navigation = { new_sequence }
-    jester.debug_log("Moving to top of stack from sequence '%s' to sequence '%s'", last_sequence, new_sequence)
-    jester.queue_sequence(new_sequence)
+    core.channel.stack.navigation = { new_sequence }
+    core.debug_log("Moving to top of stack from sequence '%s' to sequence '%s'", last_sequence, new_sequence)
+    core.queue_sequence(new_sequence)
   end
 end
 
 --[[
   Set the last item on the navigation stack as the new top.
 ]]
-function navigation_reset(action)
-  local stack = jester.channel.stack.navigation
+function _M.navigation_reset(action)
+  local stack = core.channel.stack.navigation
   if #stack == 0 then
-    jester.debug_log("Cannnot reset stack, stack is empty!")
+    core.debug_log("Cannnot reset stack, stack is empty!")
   else
     -- New stack starts with last sequence from old stack.
     new_sequence = table.remove(stack)
-    jester.channel.stack.navigation = { new_sequence }
-    jester.debug_log("Reset top of stack to sequence '%s'", new_sequence)
+    core.channel.stack.navigation = { new_sequence }
+    core.debug_log("Reset top of stack to sequence '%s'", new_sequence)
   end
 end
 
 -- Make sure module initialization only runs once.
-if not jester.modules.navigation.navigation.init_run then
-  jester.modules.navigation.navigation.init_run = true
+if not _M.init_run then
+  _M.init_run = true
   init()
 end
 
+return _M
