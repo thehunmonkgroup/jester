121775ec351eeacc75cd712c401e1bd413363c59
Chad Phillips <chad@apartmentlines.com>
2016-01-12 12:20:36 -0700

    port core actions help to ldoc.

--- modules/core_actions/core_actions.lua
+++ modules/core_actions/core_actions.lua
@@ -1,3 +1,274 @@
+--- Actions provided by jester core.
+--
+-- This module provides actions that are closely tied to the core functionality
+-- of Jester.
+--
+-- @module core_actions
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
+
+--- Execute a FreeSWITCH API command.
+--
+-- This action allows you to execute a FreeSWITCH API command, as if from the
+-- console command line. The command result is stored in the specified key.
+--
+-- @action api_command
+-- @param action
+--   api_command
+-- @param command
+--   The command to execute.
+-- @param storage_key
+--   (Optional) The key to store the command result under in the 'api_command'
+--   storage area. Default is 'result'.
+-- @usage
+--   {
+--     action = "api_command",
+--     command = "uptime",
+--     storage_key = "uptime_result",
+--   }
+
+
+--- Call a new sequence from the currently running sequence.
+--
+-- This action calls a new sequence from the currently running sequence.
+--
+-- @action call_sequence
+-- @param action
+--   call_sequence
+-- @param sequence
+--   The new sequence to call. By default the original sequence is not resumed
+--   when the new sequence completes. To resume the old sequence (effectively
+--   making the new sequence a subroutine of the original), prefix the sequence
+--   with 'sub:'. See @{sequences.md|sequences} for more information.
+-- @usage
+--   {
+--     action = "call_sequence",
+--     sequence = "foo_sequence arg1",
+--   }
+--   -- or...
+--   {
+--     action = "call_sequence",
+--     sequence = "sub:foo_sequence arg1,arg2",
+--   }
+
+
+--- Remove storage values from a given storage area.
+--
+-- This action removes key/value pairs from the given storage area.
+--
+-- @action clear_storage
+-- @param action
+--   clear_storage
+-- @param data_keys
+--   (Optional) A table of keys to remove from the storage area. If this
+--   parameter is not given, then the entire storage area is cleared, use
+--   caution!
+-- @param storage_area
+--   (Optional) The area to clear. Defaults to 'default'.
+-- @usage
+--   {
+--     action = "clear_storage",
+--     data_keys = {
+--       "key1",
+--       "key2",
+--     },
+--     storage_area = "someplace",
+--   }
+
+
+--- Call a new sequence based on a simple conditional check.
+--
+-- This action allows simple comparison of a value against another value, and
+-- can call another sequence based on if the comparison returns true or false.
+-- It's really only meant for fairly simple sequence logic -- for more complex
+-- logic see @{sequences.md|sequences} for more information.
+--
+-- @action conditional
+-- @param action
+--   conditional
+-- @param compare_to
+--   The value that you expect it to be, or for pattern matching, any valid Lua
+--   pattern (see the Lua manual for more information on Lua patterns).
+-- @param comparison
+--   (Optional) The kind of comparison to perform. Valid values are "equal",
+--   "match", "greater\_than", and "less\_than". Default is "equal".
+-- @param if_false
+--   (Optional) The sequence to call if the comparison is false.
+-- @param if_true
+--   (Optional) The sequence to call if the comparison is true.
+-- @param value
+--   The value that you want to compare.
+-- @usage
+--   {
+--     action = "conditional",
+--     value = number_of_messages,
+--     compare_to = 0,
+--     comparison = "equal",
+--     if_true = "exit",
+--     if_false = "play_messages",
+--   }
+--   -- or...
+--   {
+--     action = "conditional",
+--     value = name,
+--     compare_to = "^bob",
+--     comparison = "match",
+--     if_true = "its_bob",
+--     if_false = "not_bob",
+--   }
+
+
+--- Copy storage values from one storage area to another.
+--
+-- This action performs a complete copy of all keys in a storage area, placing
+-- them in the specified new storage area. Note that only basic data types
+-- (string, number, boolean) are copied -- all others are ignored.
+--
+-- @action copy_storage
+-- @param action
+--   copy_storage
+-- @param copy_to
+--   The storage area to copy the data to. Note that any data already existing
+--   in this area will be removed prior to the copy.
+-- @param move
+--   (Optional) If set to true, clears the data from the original storage area,
+--   which effectively makes the operation a move. Default is false.
+-- @param storage_area
+--   (Optional) The area to copy. Defaults to 'default'.
+-- @usage
+--   {
+--     action = "copy_storage",
+--     copy_to = "new_place",
+--     move = true,
+--     storage_area = "old_place",
+--   }
+
+
+--- Registers a sequence to be executed on exit.
+--
+-- This action registers a sequence to be executed after Jester has finished
+-- running all sequences related to the active call. Channel variables and
+-- storage values are available when the registered sequence is run.
+--
+-- Sequences registered here are run before the sequences registered on hangup,
+-- and are always run, regardless if the user actively hung up the call. If you
+-- want to guarantee that the sequence will run regardless of user hangup, it's
+-- best to put it here instead of in the hangup loop.
+--
+-- @action exit_sequence
+-- @param action
+--   exit_sequence
+-- @param sequence
+--   The sequence to execute.
+-- @usage
+--   {
+--     action = "exit_sequence",
+--     sequence = "perform_maintenance",
+--   }
+
+
+--- Load a profile dynamically.
+--
+-- This action allows you to load a profile dynamically from within a sequence.
+-- This can be useful for loading a 'heavier' profile only when needed from
+-- within a 'lighter' profile.
+--
+-- @action load_profile
+-- @param action
+--   load_profile
+-- @param profile
+--   The new profile to load.
+-- @param sequence
+--   (Optional) A sequence to execute after loading the profile. If this is
+--   provided, any running sequences in the current loop will be discarded, and
+--   this sequence will be run from the top of a new sequence stack.
+-- @usage
+--   {
+--     action = "load_profile",
+--     profile = "another_profile",
+--     sequence = "start",
+--   }
+
+
+--- Dummy action which does nothing.
+--
+-- This action is just a placeholder. It can be used to skip taking any action,
+-- or to provide a way to call another sequence without taking any other action.
+--
+-- @action none
+-- @param action
+--   none
+-- @usage
+--   {
+--     action = "none",
+--   }
+
+
+--- Set storage values in a given storage area.
+--
+-- This action sets key/value pairs in the given storage area.
+--
+-- @action set_storage
+-- @param action
+--   set_storage
+-- @param data
+--   A table of data to store in the storage area. Keys are the storage keys,
+--   values are the storage values.
+-- @param storage_area
+--   (Optional) The area to store the value in. Defaults to 'default'.
+-- @usage
+--   {
+--     action = "set_storage",
+--     data = {
+--       foo = "bar",
+--       baz = "bang",
+--     },
+--     storage_area = "some_storage",
+--   }
+
+
+--- Set channel variables.
+--
+-- This action sets FreeSWITCH channel variables on the channel that Jester is
+-- currently running.
+--
+-- @action set_variable
+-- @param action
+--   set_variable
+-- @param data
+--   A table of channel variables to set. Keys are the variable
+--   names, values are the variable values.
+-- @usage
+--   {
+--     action = "set_variable",
+--     data = {
+--       foo = "bar",
+--       baz = "bang",
+--     },
+--   }
+
+
+--- Wait before performing the next action.
+--
+-- This action causes Jester to wait before continuing on. Silence is streamed
+-- on the channel during the wait period.
+--
+-- @action wait
+-- @param action
+--   wait
+-- @param keys
+--   (Optional) See keys documentation in @{sequences.md|sequences}.
+-- @param milliseconds
+--   The number of milliseconds to wait.
+-- @usage
+--   {
+--     action = "wait",
+--     keys = profile.some_keys_setting,
+--     milliseconds = 3000,
+--   }
+
+
 local core = require "jester.core"
 
 local _M = {}
