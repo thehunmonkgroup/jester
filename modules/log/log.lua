d538734090b33f86b78f8765f842234b7cc8f3b5
Chad Phillips <chad@apartmentlines.com>
2016-01-14 16:24:58 -0700

    port log module help to ldoc.

--- modules/log/log.lua
+++ modules/log/log.lua
@@ -1,3 +1,76 @@
+--- Custom logger for sequences.
+--
+-- This module provides custom logging functionality for sequences. It can be
+-- used to log data somewhere from within a sequence.
+--
+-- @module log
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
+
+--- The console handler (default).
+--
+--  Logs to the FreeSWITCH console.
+--
+--  When using this handler, the 'level' argument for the action can be any
+--  valid level used by
+--  [freeswitch.consoleLog](https://freeswitch.org/confluence/display/FREESWITCH/Lua+API+Reference#LuaAPIReference-freeswitch.consoleLog)
+--
+-- @handler console
+-- @usage
+--   {
+--     action = "log",
+--     handler = "console",
+--     level = "info",
+--     -- other params...
+--   }
+
+
+--- The file handler.
+--
+--  Logs to a file on the local filesystem.
+--
+--  When using this handler, the 'level' argument for the action can be any
+--  arbitrary value the user decides.
+--
+-- @handler file
+-- @usage
+--   {
+--     action = "log",
+--     handler = "file",
+--     level = "WARN",
+--     -- other params...
+--   }
+
+
+--- Log a custom message.
+--
+-- Allows logging a message from within a sequence, with a custom level.
+--
+-- @action log
+-- @string action
+--   log
+-- @string file
+--   (Optional) Required only for handlers that log to a file. Provide a full
+--   path to the file. Default is '/tmp/jester.log'.
+-- @string level
+--   (Optional) The log level of the message. This value will vary depending on
+--   the handler, see [handlers](#Handlers). Default is 'info'.
+-- @string message
+--   The message to log.
+-- @string handler
+--   The handler to use, see [handlers](#Handlers). If not specified, defaults
+--   to the default handler for the module.
+-- @usage
+--   {
+--     action = "log",
+--     file = "/tmp/jester.log",
+--     level = "info",
+--     message = "A custom log message",
+--     handler = "file",
+--   }
+
+
 local core = require "jester.core"
 
 local _M = {}
