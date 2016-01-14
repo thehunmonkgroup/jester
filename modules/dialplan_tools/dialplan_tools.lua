6ecededd7b8581a51b5b5b1b674e97646963c41d
Chad Phillips <chad@apartmentlines.com>
2016-01-13 23:03:20 -0700

    port dialplan tools module help to ldoc.

--- modules/dialplan_tools/dialplan_tools.lua
+++ modules/dialplan_tools/dialplan_tools.lua
@@ -1,3 +1,108 @@
+--- Provides access to dialplan applications.
+--
+-- This module provides access to various dialplan applications. An
+-- understanding of how these applications are used in FreeSWITCH dialplans is
+-- essential in order to properly use the actions in this module.
+--
+-- @module dialplan_tools
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
+
+--- Bridges the current Jester channel with another endpoint.
+--
+-- Note that many characteristics of the bridge can be controlled by setting
+-- various channel variables prior to the bridge. Check the FreeSWITCH wiki for
+-- more information on the available channel variables, and
+-- @{core_actions.set_variable} for setting channel variables from Jester.
+--
+-- @action bridge
+-- @string action
+--   bridge
+-- @string channel
+--   The channel to use for the bridge. Can be a string (used for all
+--   extensions), or optionally a table of multiple channels (used with the
+--   matching extension in the extension table). Be sure to include everything
+--   up to the actual extension, including trailing slash and any dial prefix.
+-- @string extension
+--   The extension to bridge to. Can be a string (used for all channels), or
+--   optionally a table of multiple extensions (used with the matching channel
+--   in the channel table).
+-- @bool hangup_after_bridge
+--   (Optional) If set to true, the call will be hungup after the bridge
+--   completes. Note that all sequences registered for the exit and hangup
+--   loops will still be run.
+-- @string multichannel_type
+--   (Optional) If multiple channels or extensions are specified, this setting
+--   determines how they will be connected. 'first\_wins' rings all channels
+--   until the first responds with media, then bridges that channel.
+--   'sequential' rings each channel in succession, bridging the first one that
+--   responds with media. Default is 'first\_wins'.
+-- @tab variables
+--   A table of channel variables to set for the bridge, key = variable name,
+--   value = variable value. Can be a single table (used for all channels), or
+--   optionally a table of variable tables (used with the matching channel in
+--   the channel list).
+-- @usage
+--   {
+--     action = "bridge",
+--     channel = "sofia/internal/",
+--     extension = {
+--       "1005",
+--       "1006",
+--     },
+--     hangup_after_bridge = true,
+--     multichannel_type = "first_wins",
+--     variables = {
+--       foo = "bar",
+--     },
+--   }
+
+
+--- Execute dialplan applications.
+--
+-- This action provides access to execute any dialplan application via Jester.
+-- Note that key presses will not be recognized during the execute action, use
+-- the various play and record actions to allow capturing of key presses.
+--
+-- @action execute
+-- @string action
+--   execute
+-- @string application
+--   The application to execute.
+-- @string data
+--   (Optional) Data to pass to the application.
+-- @usage
+--   {
+--     action = "execute",
+--     application = "execute_extension",
+--     data = "set_up_call",
+--   }
+
+
+--- Transfer to another extension in the dialplan.
+--
+-- This action is used to transfer to another extension in the dialplan. Prior
+-- to the transfer, the active sequence stack is cleared.
+--
+-- @action transfer
+-- @string action
+--   transfer
+-- @string context
+--   (Optional) The context to transfer to. Default is the current context.
+-- @string dialplan
+--   (Optional) The dialplan to transfer to. Default is 'XML'.
+-- @string extension
+--   The extension to transfer to.
+-- @usage
+--   {
+--     action = "transfer",
+--     context = "example",
+--     dialplan = "XML",
+--     extension = "101",
+--   }
+
+
 local core = require "jester.core"
 
 local _M = {}
