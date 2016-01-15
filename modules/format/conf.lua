3765aebdd59432cd58eca0fd4b6991dcbbf6e659
Chad Phillips <chad@apartmentlines.com>
2016-01-14 17:08:51 -0700

    provide alias for format_number to format_string action for backwards compat.

--- modules/format/conf.lua
+++ modules/format/conf.lua
@@ -1,6 +1,12 @@
 local conf = {
   action_map = {
 
+    -- Alias for format_string, to maintain backwards compat.
+    format_number = {
+      mod = "format",
+      func = "format_string",
+    },
+
     format_string = {
       mod = "format",
       func = "format_string",
