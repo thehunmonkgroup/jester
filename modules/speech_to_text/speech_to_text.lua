f66cd8b460fcadae9bebd197287b772e373ab720
Chad Phillips <chad@apartmentlines.com>
2016-01-15 08:32:03 -0700

    port speech to text module help to ldoc.

--- modules/speech_to_text/speech_to_text.lua
+++ modules/speech_to_text/speech_to_text.lua
@@ -1,3 +1,77 @@
+--- Speech to text translation (experimental).
+--
+-- **WARNING:** totally experimental, no guarantees it will work!
+--
+-- This module provides speech to text translation.
+--
+-- The module requires the Lua
+-- [lua-cjson](https://luarocks.org/modules/luarocks/lua-cjson) and
+-- [luasec](https://luarocks.org/modules/brunoos/luasec) packages.
+--
+-- @module speech_to_text
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
+
+--- The AT&T handler (default).
+--
+--  Uses AT&T's Speech to Text service. The service requires a valid developer
+--  account and application key/secret, see
+--  [here](https://developer.att.com/apis/speech) for more information.
+--
+-- @handler att
+-- @usage
+--   {
+--     action = "speech_to_text_from_file",
+--     handler = "att",
+--     -- other params...
+--   }
+
+
+--- Speech to text storage.
+--
+-- Translations are stored in the specified storage area with the following
+-- keys, where <code>X</code> is the chunk number:
+--
+-- A 'status' key is also placed in the storage area, indicating the result of
+-- the translation. A value of 0 indicates the translation was successful.
+--
+-- @table speech_to_text
+--
+-- @field translation_X
+--   The translated text for the chunk.
+-- @field confidence_X
+--   The confidence level of the translated chunk, a decimal number in the
+--   range of 0 to 1.
+
+
+--- Translates a sound file to text.
+--
+-- This action requires that [flac](https://xiph.org/flac) is installed and
+-- executable by FreeSWITCH.
+--
+-- @action speech_to_text_from_file
+-- @string action
+--   speech\_to\_text\_from\_file
+-- @string app_key
+--   (Optional) The application key used to access the service.
+-- @string app_secret
+--   (Optional) The application secret used to access the service.
+-- @string filepath
+--   The full path to the file to translate.
+-- @string storage_area
+--   (Optional) The storage area to store the response in. Defaults to
+--   'speech\_to\_text'.
+-- @usage
+--   {
+--     action = "speech_to_text_from_file",
+--     app_key = profile.speech_to_text_app_key,
+--     app_secret = profile.speech_to_text_app_secret,
+--     filepatch = "/tmp/foo.wav",
+--     storage_area = "foo_to_text",
+--   }
+
+
 local core = require "jester.core"
 
 local io = require("io")
