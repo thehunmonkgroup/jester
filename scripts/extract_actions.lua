a423610a4d2ac4006e8384172118ce500369b534
Chad Phillips <chad@apartmentlines.com>
2016-01-17 19:03:05 -0700

    output action map as full lua module.

--- scripts/extract_actions.lua
+++ scripts/extract_actions.lua
@@ -1,4 +1,18 @@
-local json = require "cjson"
+local filename = "action_map.lua"
+
+local output = [[
+--- This module represents Jester actions as extracted from Jester using ldoc.
+--
+-- It is a simple map of all actions, their parameters, and what value type
+-- the parameter accepts.
+--
+-- @module action_map
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
+return {
+]]
+
 return {
   filter = function (t)
     local data = {}
@@ -6,7 +20,7 @@ return {
       for _, item in ipairs(mod.items) do
         if item.type == 'action' then
           local action = item.name
-          data[action] = {}
+          output = output .. "  " .. action .. " = {\n"
           --[[
           print("------------")
           for k, v in pairs(item) do
@@ -20,13 +34,18 @@ return {
           ]]
           for _, param in ipairs(item.params) do
             if param ~= "handler" then
-              data[action][param] = item.modifiers.param[param].type
+              output = output .. "    " .. param .. ' = "' .. item.modifiers.param[param].type .. '"' .. ",\n"
             end
           end
+          output = output .. "  },\n"
         end
       end
     end
-    local encoded = json.encode(data)
-    print(encoded)
+    output = output .. "}"
+    local file, err = io.open(filename, "wb")
+    if err then print(err) end
+    file:write(output)
+    file:close()
+    print ("actions extracted to " .. filename);
   end
 }
