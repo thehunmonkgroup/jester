41a8ef6c0ea547da183d47427162a629245749b5
Chad Phillips <chad@apartmentlines.com>
2016-01-17 19:42:39 -0700

    doc cleanup, remove action param from actions in action map.

--- scripts/extract_actions.lua
+++ scripts/extract_actions.lua
@@ -1,10 +1,23 @@
+--- Extracts Jester module actions using ldoc.
+--
+-- This script extracts a simple map of all Jester actions into a small module
+-- that can be used by other modules/scripts needing a global map which
+-- includes all parameters and their value types.
+--
+-- To re-generate the map, run the following from the root Jester directory:
+-- <code>ldoc --filter scripts.extract_actions.filter .</code>
+--
+-- @script extract_actions.lua
+-- @author Chad Phillips
+-- @copyright 2011-2015 Chad Phillips
+
 local filename = "action_map.lua"
 
 local output = [[
 --- This module represents Jester actions as extracted from Jester using ldoc.
 --
--- It is a simple map of all actions, their parameters, and what value type
--- the parameter accepts.
+-- It is a simple map of all actions, their parameters (minus the action
+-- itself), and what value type the parameter accepts.
 --
 -- @module action_map
 -- @author Chad Phillips
@@ -33,8 +46,8 @@ return {
           print("------------")
           ]]
           for _, param in ipairs(item.params) do
-            if param ~= "handler" then
-              output = output .. "    " .. param .. ' = "' .. item.modifiers.param[param].type .. '"' .. ",\n"
+            if param ~= "action" and param ~= "handler" then
+              output = output .. "    " .. param .. [[ = "]] .. item.modifiers.param[param].type .. [[",]] .. "\n"
             end
           end
           output = output .. "  },\n"
