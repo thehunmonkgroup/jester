e0004ffa880ae4305432b14cfc53bc6d62f98ce2
Chad Phillips <chad@apartmentlines.com>
2021-02-22 14:33:30 -0700

    add table.stringify support method

--- support/table.lua
+++ support/table.lua
@@ -1,3 +1,5 @@
+local url = require("socket.url")
+
 --[[
   Given an associative array table, returns a new table with an ordered
   list of the values of the passed table.
@@ -24,3 +26,22 @@ function table.ordervalues(t)
   return list
 end
 
+function table.stringify(params, sep, eq)
+  if not sep then sep = '&' end
+  if not eq then eq = '=' end
+  if type(params) == "table" then
+    local fields = {}
+    for key,value in pairs(params) do
+      local keyString = url.escape(tostring(key)) .. eq
+      if type(value) == "table" then
+        for _, v in ipairs(value) do
+          table.insert(fields, keyString .. url.escape(tostring(v)))
+        end
+      else
+        table.insert(fields, keyString .. url.escape(tostring(value)))
+      end
+    end
+    return table.concat(fields, sep)
+  end
+  return ''
+end
