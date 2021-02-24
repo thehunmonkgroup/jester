2b4288262fa8aa5b9e03847d0d76deaecc8da43d
Chad Phillips <chad@apartmentlines.com>
2021-02-23 22:48:27 -0700

    refactor base speech to text method into smaller functions

--- modules/speech_to_text/init.lua
+++ modules/speech_to_text/init.lua
@@ -47,6 +47,79 @@ local DEFAULT_FILE_TYPE = "audio/wav"
 
 local _M = {}
 
+local function check_filepath(filepath)
+  if filepath then
+    return true
+  else
+    return false, "ERROR: Missing filepath"
+  end
+end
+
+local function load_file(arguments)
+  local filepath = arguments.filepath
+  local file_type = arguments.file_type or DEFAULT_FILE_TYPE
+  local content_length
+  local file, data = load_file(filepath)
+  if file then
+    local attributes = {
+      file = file,
+      file_type = file_type,
+      content_length = data.filesize
+    }
+    return file, data
+  else
+    return false, string.format([[ERROR: could not open '%s': %s]], filepath, data)
+  end
+end
+
+local function parse_response(handler, data)
+  local success, data = handler.parse_transcriptions(data)
+  if success then
+    return success, data
+  else
+    return false, string.format([[ERROR: Parsing Speech to Text API response failed: %s]], data)
+  end
+end
+
+local function make_request_using_handler(handler, arguments, attributes)
+  local success, data = handler.make_request(arguments, attributes)
+  if success then
+    return parse_response(handler, data)
+  else
+    return false, string.format([[ERROR: Speech to Text API failed: %s]], data)
+  end
+end
+
+local function make_request(arguments, handler)
+  local success, data = load_file(arguments)
+  if success then
+    success, data = make_request_using_handler(handler, arguments, data)
+  end
+  return success, data
+end
+
+local function retry_wait(arguments, attempt)
+  local retries = arguments.retries or DEFAULT_RETRIES
+  local retry_wait_seconds = arguments.retry_wait_seconds or DEFAULT_RETRY_WAIT_SECONDS
+  if attempt < retries then
+    core.debug_log([[ERROR: Attempt #%d failed, re-trying Speech to Text API in %d seconds]], attempt, retry_wait_seconds)
+    socket.sleep(retry_wait_seconds)
+  end
+end
+
+local function make_request_with_retry(arguments, handler)
+  local success, data
+  for attempt = 1, retries do
+    success, data = make_request(arguments, handler)
+    if success then
+      break
+    else
+      retry_wait(arguments, attempt)
+    end
+  end
+  return success, data
+end
+
 --- Translates a sound file to text.
 --
 -- @string api_key
@@ -60,47 +133,12 @@ local _M = {}
 --     storage_area = "foo_to_text",
 --   }
 function _M.speech_to_text_from_file(arguments, handler)
-
-  local filepath = arguments.filepath
-  local file_type = arguments.file_type or DEFAULT_FILE_TYPE
-  handler = handler or DEFAULT_HANDLER
-  local status = 1
-  local transcriptions = {}
-  if filepath then
-    for i = 1, retries do
-      local content_length
-      local file, data = load_file(filepath)
-      if file then
-        local attributes = {
-          file = file,
-          file_type = file_type,
-          content_length = data.filesize
-        }
-        local success, data = handler.make_request(arguments, attributes)
-        if success then
-          success, data = handler.parse_transcriptions(data)
-          if success then
-            status = 0
-            transcriptions = data
-            return status, transcriptions
-          else
-            core.debug_log("ERROR: Parsing Speech to Text API response failed: %s", data)
-          end
-        else
-          core.debug_log([[ERROR: Speech to Text API attempt #%d failed: %s]], i, data)
-        end
-      else
-        core.debug_log("ERROR: could not open '%s': %s", filepath, data)
-      end
-      if i < retries then
-        core.debug_log([[ERROR: Re-trying Speech to Text API in %d seconds]], retry_wait_seconds)
-        socket.sleep(retry_wait_seconds)
-      end
-    end
-  else
-    core.debug_log("ERROR: Missing filepath")
+  local success, data = check_filepath(arguments.filepath)
+  if success then
+    handler = handler or DEFAULT_HANDLER
+    success, data = make_request_with_retry(arguments, handler)
   end
-  return status, transcriptions
+  return success, data
 end
 
 return _M
