901ca00e4477c55df1b5130eda6c7605a729e68d
Chad Phillips <chad@apartmentlines.com>
2021-03-07 00:02:18 -0700

    add file support funcs

--- support/file.lua
+++ support/file.lua
@@ -43,3 +43,33 @@ function load_file(filepath)
   end
   return file, data
 end
+
+--[[
+  Write file.
+]]
+function write_file(filepath, data, mode)
+  mode = mode and mode or "w"
+  local file
+  local success, err = false, nil
+  file, err = io.open(filepath, mode)
+  if file then
+    success, err = file:write(data)
+    if success then
+      success = true
+    end
+    file:close()
+  end
+  return success, err
+end
+
+--[[
+  Removes file.
+]]
+function remove_file(filepath)
+  local ok, err = os.remove(filepath)
+  if ok then
+    debug_print(string.format("Removed filepath: %s", filepath))
+  else
+    error_print(string.format("ERROR: could not remove filepath %s: %s", filepath, err))
+  end
+end
