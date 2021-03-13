b5ee947a3245e0a28b370c7cbec35327136fe60b
Chad Phillips <chad@apartmentlines.com>
2021-03-12 19:54:14 -0700

    allow example speech to text to easily store api keys and switch handlers

--- examples/speech_to_text.lua
+++ examples/speech_to_text.lua
@@ -1,38 +1,56 @@
+--[[
+  Add examples/private.lua in same directory as this file, with these
+  global variables set:
+
+  Rev.ai
+    REV_AI_API_KEY
+
+  Watson
+    WATSON_API_KEY
+    WATSON_SERVICE_URI
+]]
+require("jester.examples.private")
+
 local inspect = require("inspect")
 local core = require "jester.core"
 local stt = require "jester.modules.speech_to_text"
 local rev_ai = require "jester.modules.speech_to_text.rev_ai"
 local watson = require "jester.modules.speech_to_text.watson"
 
-local handler = rev_ai
---local handler = watson
+local DEFAULT_HANDLER = "watson"
 
-local API_KEY = "[API KEY]"
-local SERVICE_URI = "[SERVICE URL]"
+local rev_ai_handler = rev_ai:new({
+  api_key = REV_AI_API_KEY,
+  options = {
+    speaker_channels_count = 1,
+    remove_disfluencies = true,
+    delete_after_seconds = 120,
+  },
+})
+local watson_handler = watson:new({
+  api_key = WATSON_API_KEY,
+  service_uri = WATSON_SERVICE_URI,
+  query_parameters = {
+    model = "en-US_NarrowbandModel",
+    smart_formatting = true,
+    split_transcript_at_phrase_end = true,
+  },
+})
 
-function speech_to_text_from_file(filepath)
+function speech_to_text_from_file(filepath, handler)
   local params = {
-    api_key = API_KEY,
-    service_uri = SERVICE_URI,
-    filepath = filepath,
-    options = {
-      speaker_channels_count = 1,
-      remove_disfluencies = true,
-      delete_after_seconds = 120,
-    },
-    query_parameters = {
-      model = "en-US_NarrowbandModel",
-      smart_formatting = true,
-      split_transcript_at_phrase_end = true,
-    },
     retries = 10,
     retry_wait_seconds = 30,
     timeout_seconds = 300,
   }
+  local file_params = {
+    path = filepath,
+  }
   local confidence, text
-  local success, data = stt.speech_to_text_from_file(params, handler)
+  local stt_obj = stt:new(handler, params)
+  local success, data = stt_obj:speech_to_text_from_file(file_params)
   if success then
-    confidence, text = handler.transcriptions_to_text(data)
+    confidence, text = handler:transcriptions_to_text(data)
   else
     core.log.err(data)
   end
@@ -40,8 +58,15 @@ function speech_to_text_from_file(filepath)
 end
 
 local filepath = arg[1]
+local handler_arg = arg[2] or DEFAULT_HANDLER
+local handler
+if handler_arg == "watson" then
+  handler = watson_handler
+elseif handler_arg == "revai" then
+  handler = rev_ai_handler
+end
 core.bootstrap()
-local success, data, confidence, text = speech_to_text_from_file(filepath)
+local success, data, confidence, text = speech_to_text_from_file(filepath, handler)
 if success then
   core.log.info("RAW DATA: \n\n%s", inspect(data))
   core.log.info("Confidence in transcription: %.2f%%\n", confidence)
